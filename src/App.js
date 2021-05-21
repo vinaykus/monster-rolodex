9be9160242aa59f93cd2585ff5301d39ede60252
Vinay Kushwaha <Vinay_Kushwaha@hotmail.com>
2021-05-21 16:53:37 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import  { CardList} from './components/card-list/card-list.component.jsx';
+import { SearchBox } from './components/search-box/search-box.component.jsx';
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters:[],
+      searchField:''
+    }
+    //this.handleChange =  this.handleChange.bind(this);
+  }
+  handleChange = e =>{
+      this.setState({searchField : e.target.value })
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters :users}));
+  }
+  render(){
+    const {monsters, searchField} = this.state;
+    const filteredMonster = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox placeholder='search monster' handleChange = {this.handleChange}  />
+        <CardList monsters = {filteredMonster}/>  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
